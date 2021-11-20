edc7189d6bc4e3bb8cac747fcdf5331184aea137
LBugnion <LBugnion@outlook.com>
2021-11-20 12:23:35 +0100

    First commit, works locally

---
+++ ChatServer/ChatServer/Negotiate.cs
@@ -0,0 +1,29 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace ChatServer
+{
+    public static class Negotiate
+    {
+        [FunctionName(nameof(Negotiate))]
+        public static SignalRConnectionInfo Run(
+            [HttpTrigger(
+                AuthorizationLevel.Anonymous,
+                "get",
+                Route = "negotiate")]
+            HttpRequest req,
+            [SignalRConnectionInfo(HubName = "simplechat")]
+            SignalRConnectionInfo connectionInfo,
+            ILogger log)
+        {
+            return connectionInfo;
+        }
+    }
+}
