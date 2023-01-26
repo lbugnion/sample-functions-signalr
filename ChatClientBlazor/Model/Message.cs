cebb0c44cab95b6e1d8ec45ca084fd0b87cff09b
Laurent Bugnion <lbugnion@microsoft.com>
2023-01-26 15:16:19 +0100

    Adding a Blazor client

---
+++ ChatClientBlazor/Model/Message.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatClientBlazor.Model
+{
+    public class Message
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Text { get; set; } = string.Empty;
+    }
+}
---
+++ ChatClientBlazor/Pages/Index.razor.cs
@@ -0,0 +1,30 @@
+﻿using ChatClientBlazor.Model;
+using Microsoft.JSInterop;
+
+namespace ChatClientBlazor.Pages
+{
+    public partial class Index
+    {
+#if DEBUG
+        public const string ApiBaseUrl = "http://localhost:7071";
+#else
+        public const string ApiBaseUrl = "";
+#endif
+
+        public bool Ready { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+
+        public List<Message> Messages { get; set; } = new List<Message>();
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                UserName = await JSRuntime.InvokeAsync<string>("askUserName");
+                Ready = true;
+                StateHasChanged();
+            }
+        }
+    }
+}
